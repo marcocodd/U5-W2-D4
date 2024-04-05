0ed8ba623609b7dff364f48dda4f300d11841d1e
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-05 16:40:18 +0200

    creato progetto spring

--- src/main/java/marco/U5W2D4/U5W2D4Application.java
+++ src/main/java/marco/U5W2D4/U5W2D4Application.java
@@ -1,5 +1,6 @@
 package marco.U5W2D4;
 
+import marco.U5W2D4.entities.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -7,15 +8,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootApplication
 public class U5W2D4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(com.epicode.U5D1.U5D1Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(marco.U5W2D4.U5W2D4Application.class, args);
 
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5W2D4Application.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5W2D4Application.class);
 
-		Menu m = (com.epicode.U5D1.entities.Menu) ctx.getBean("menu");
+        Menu m = (marco.U5W2D4.entities.Menu) ctx.getBean("menu");
 
-		m.printMenu();
-		ctx.close();
-	}
+        m.printMenu();
+        ctx.close();
+    }
 
 }
